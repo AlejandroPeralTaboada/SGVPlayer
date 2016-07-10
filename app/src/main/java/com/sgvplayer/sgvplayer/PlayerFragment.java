cee675d8aec5e0bc25ae85a3af4e8a446b2e7e6d
Alvaro Furlan <af15070@my.bristol.ac.uk>
2016-07-10 19:23:29 +0200

    PlayerFragment successfully receives MP3File

--- app/src/main/java/com/sgvplayer/sgvplayer/PlayerFragment.java
+++ app/src/main/java/com/sgvplayer/sgvplayer/PlayerFragment.java
@@ -7,9 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.sgvplayer.sgvplayer.model.fileNavigator.MP3File;
+
+import java.io.Serializable;
 
 
-/**
+ /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link PlayerFragment.OnFragmentInteractionListener} interface
@@ -20,12 +26,9 @@ import android.view.ViewGroup;
 public class PlayerFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+     private static final String ARG_MP3FILE = "mp3File";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MP3File mp3File;
 
     private OnFragmentInteractionListener mListener;
 
@@ -36,17 +39,14 @@ public class PlayerFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param mp3File File to play.
      * @return A new instance of fragment PlayerFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PlayerFragment newInstance(String param1, String param2) {
+    public static PlayerFragment newInstance(Serializable mp3File) {
         PlayerFragment fragment = new PlayerFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(ARG_MP3FILE, mp3File);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,8 +55,7 @@ public class PlayerFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mp3File = (MP3File) getArguments().getSerializable(ARG_MP3FILE);
         }
     }
 
@@ -64,7 +63,12 @@ public class PlayerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false);
+        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        String name = "Playing" + this.mp3File.getFile().getName();
+        TextView fileName = (TextView) view.findViewById(R.id.file_name);
+        fileName.setText(name);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
