42a586aca266ca752ecb090c0fe84c6326bc52cf
Alvaro Furlan <af15070@my.bristol.ac.uk>
2016-07-14 09:49:43 +0200

    Working in PlayerActivity

--- app/src/main/java/com/sgvplayer/sgvplayer/PlayerActivity.java
+++ app/src/main/java/com/sgvplayer/sgvplayer/PlayerActivity.java
@@ -7,7 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class PlayerActivity extends AppCompatActivity {
+import com.sgvplayer.sgvplayer.model.fileNavigator.MP3File;
+import com.sgvplayer.sgvplayer.model.mp3Service.Mp3Service;
+import com.sgvplayer.sgvplayer.model.mp3Service.Mp3ServiceProvided;
+
+public class PlayerActivity extends AppCompatActivity
+        implements Mp3ServiceProvided {
+
+    private MP3File mp3File;
+    private Mp3Service mp3Service; //need to add to bundle--MAYBE NOT
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +23,15 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    //Media Player methods:
+    //Called by Mp3ServiceProvider
+    @Override
+    public void onServiceConnected(Mp3Service mp3Service){
+        //implement onServiceConnected
+        this.mp3Service = mp3Service;
+        this.mp3Service.playSong(mp3File);
     }
 
 }
